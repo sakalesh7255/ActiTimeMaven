e67b2645fbd06d23a5071e47102454019484fa51
arka2 <arka2@arka2-PC>
2021-02-10 14:12:52 +0530

    My First git hub repository

---
+++ ActiTimeMaven/src/main/java/com/frame/generics/AutoConstant.java
@@ -0,0 +1,20 @@
+package com.frame.generics;
+
+public interface AutoConstant {
+	
+	String chrome_key = "webdriver.chrome.driver";
+	String chrome_value = ".src/test/resources/driver/chromedriver.exe";
+	
+	String gecko_key = "webdriver.gecko.driver";
+	String gecko_value =".src/test/resources/driver/geckodriver.exe";
+	
+	String URL = "http://localhost:8080/login.do";
+	
+	String Read_XL_data = ".src/test/resources/testdata/input.xlsx";
+	String Photo_Path = "./photo/";
+	String Report_Path = ".src/test/resources/results/report.xlsx";
+	
+	long ito = 10;
+	long eto = 20;
+
+}
